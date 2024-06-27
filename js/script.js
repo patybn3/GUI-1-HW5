78a7ddaa6360f3799660c4b64fe52e3d0b89721e
patybn3 <paty.nigro@gmail.com>
2024-06-27 14:35:44 -0400

    logic started, half way there I think. I changed the path to the js file on html, and added a blank image to drop the tiles on

--- js/script.js
+++ js/script.js
@@ -50,6 +50,141 @@ $(document).ready(() => {
     loadDroppableTargets();
   }
 
-  function laodScrabblePieces(){};
-
-  function loadDroppableTargets(){};
+  function findWord() {
+    let word = "";
+    let wordScore = 0;
+    let doubleWordScore = false;
+  
+    gameBoard.forEach((slot, index) => {
+      if (slot.tile !== "tileX") {
+        const letter = findLetter(slot.tile);
+        word += letter;
+        let letterScore = findScore(slot.tile);
+  
+        // Check if the tile is on a double letter score
+        if (index === 2 || index === 8) {
+          letterScore *= 2;
+        }
+  
+        wordScore += letterScore;
+  
+        // Check if the tile is on a double word score
+        if (index === 6 || index === 12) {
+          doubleWordScore = true;
+        }
+      }
+    });
+  
+    // Apply double word score if applicable
+    if (doubleWordScore) {
+      wordScore *= 2;
+    }
+  
+    // Update total score and display it
+    totalScore += wordScore;
+    $("#score").html(totalScore);
+    $("#validationMessage").html(word || "____");
+  }
+  
+  function findScore(tileId) {
+    const letter = findLetter(tileId);
+    const piece = scrabblePieces.find(piece => piece.letter === letter);
+    return piece ? piece.value : 0;
+  }
+  
+  function findLetter(tileId) {
+    const tile = gameTiles.find(tile => tile.id === tileId);
+    return tile ? tile.letter : "";
+  }
+  
+  function findBoardPosition(dropId) {
+    return gameBoard.findIndex(slot => slot.id === dropId);
+  }
+  
+  function findTilePosition(tileId) {
+    const slot = gameBoard.find(slot => slot.tile === tileId);
+    return slot ? slot.id : -1;
+  }
+  
+  function loadScrabblePieces() {
+    const baseUrl = "graphics_data/Scrabble_Tile_";
+  
+    for (let i = 0; i < 7; i++) {
+      let randomNum;
+      let letter;
+  
+      do {
+        randomNum = getRandomInt(0, scrabblePieces.length - 1);
+        letter = scrabblePieces[randomNum].letter;
+      } while (scrabblePieces[randomNum].amount === 0);
+  
+      scrabblePieces[randomNum].amount--;
+  
+      const pieceHtml = `<img class="tile" id="tile${i}" src="${baseUrl}${letter}.jpg" alt="${letter}">`;
+      $("#rackTiles").append(pieceHtml);
+  
+      $(`#tile${i}`).css({
+        "position": "absolute",
+        "left": (50 * i) + "px",
+        "top": "0px"
+      }).draggable();
+  
+      gameTiles[i].letter = letter;
+    }
+  }
+  
+  function loadDroppableTargets() {
+    const imgUrl = "graohics_data/Scrabble_Blank.png";
+    
+    // for (let i = 0; i < 15; i++) {
+    const dropHtml = `<img class="droppable" id="drop${i}" src="${imgUrl}" alt="Drop Zone">`;
+    $("#scrabbleBoard").append(dropHtml);
+  
+      $(`#drop${i}`).css({
+        
+      }).droppable({
+        drop: function (event, ui) {
+          const draggableId = ui.draggable.attr("id");
+          const droppableId = $(this).attr("id");
+  
+          console.log(`Tile: ${draggableId} - dropped on ${droppableId}`);
+  
+          gameBoard[findBoardPosition(droppableId)].tile = draggableId;
+          findWord();
+        },
+        out: function (event, ui) {
+          const draggableId = ui.draggable.attr("id");
+          const droppableId = $(this).attr("id");
+  
+          if (draggableId !== gameBoard[findBoardPosition(droppableId)].tile) {
+            console.log("FALSE ALARM DETECTED.");
+            return;
+          }
+  
+          console.log(`Tile: ${draggableId} - removed from ${droppableId}`);
+          gameBoard[findBoardPosition(droppableId)].tile = "tileX";
+          findWord();
+        }
+      });
+    // }
+  }
+  
+  function submitWord() {
+    // Functionality to submit the word
+    console.log("Word submitted");
+  }
+  
+  function resetBoard() {
+    // Functionality to reset the board
+    console.log("Board reset");
+  }
+  
+  function getNewTiles() {
+    // Functionality to get new tiles
+    console.log("New tiles generated");
+  }
+  
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
